c7dd0cc6346611cea6a25f4ab0f8cd681233dfa4
dcihak <dcihak@dhcp-10-40-5-88.brq.redhat.com>
2018-10-05 14:45:10 +0200

    [WFLY-10336] Service implements SOAPHandler to fix MustUnderstand headers issue

--- testsuite/integration/ws/src/test/java/org/jboss/as/test/integration/ws/authentication/policy/resources/EchoService.java
+++ testsuite/integration/ws/src/test/java/org/jboss/as/test/integration/ws/authentication/policy/resources/EchoService.java
@@ -73,11 +73,12 @@ public class EchoService implements EchoServiceRemote {
 
     @Override
     public Set<QName> getHeaders() {
-        QName securityHeader = new QName("http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd",
-                "Security");
-        HashSet<QName> headers = new HashSet<QName>();
-        headers.add(securityHeader);
-        return headers;
+        return null;
+//        QName securityHeader = new QName("http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd",
+//                "Security");
+//        HashSet<QName> headers = new HashSet<QName>();
+//        headers.add(securityHeader);
+//        return headers;
     }
 
     @Override
